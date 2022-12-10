5c6622e4bbab96bc3f2d95743d8b88734bfcadca
David Ernst <git@dsernst.com>
2022-12-09 23:02:22 -0800

    Show ui feedback when pressing Register btn

--- src/RegistrationForm.tsx
+++ src/RegistrationForm.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import { FormItem } from './FormItem'
 
 export const RegistrationForm = () => {
+  const [submitted, setSubmitted] = useState(false)
+
   return (
     <form className="w-full px-4 mt-1 text-left rounded-lg shadow-xl bg-orange-50">
       <FormItem label="Your Name" />
@@ -9,8 +12,12 @@ export const RegistrationForm = () => {
 
       <FormItem label="Your Email Address" />
 
-      <a className="block text-center py-1.5 my-5 bg-amber-200/75 rounded-md cursor-pointer font-medium border-amber-400 border hover:bg-amber-300">
-        Submit
+      {/* Register btn */}
+      <a
+        className="block text-center py-1.5 my-5 bg-amber-200/75 rounded-md cursor-pointer font-medium border-amber-400 border hover:bg-amber-300 shadow-md active:bg-amber-400 active:shadow-sm"
+        onClick={() => setSubmitted(!submitted)}
+      >
+        {!submitted ? 'Register' : "Nothing's being saved yet! 🙂"}
       </a>
     </form>
   )
