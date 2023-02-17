d50d51ec1b3c79c013c67c5850e13e3a60620eb3
David Ernst <git@dsernst.com>
2023-02-17 10:57:21 -0800

    Add hover:subtle bg darken form registration form

--- src/FormItem.tsx
+++ src/FormItem.tsx
@@ -11,7 +11,7 @@ export const FormItem = ({
     <label className="block mt-4 text-sm font-medium font-form tracking-[1px] text-zinc-600 uppercase">
       {label}{' '}
       <input
-        className="w-full px-1 mt-0.5 text-base font-medium bg-white border-2 border-zinc-600 focus:border-magenta-pink focus:ring-0 focus:outline-none"
+        className="w-full px-1 mt-0.5 text-base font-medium bg-white border-2 border-zinc-600 hover:bg-slate-50 focus:border-magenta-pink focus:ring-0 focus:outline-none"
         {...{ value }}
         onChange={({ target }) => {
           setter(target.value)
