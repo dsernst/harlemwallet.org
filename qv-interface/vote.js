8b5e3170af983ab04dd7301dafe4850456a74801
David Ernst <git@dsernst.com>
2023-03-03 18:44:40 -0800

    Placeholder loader

--- qv-interface/vote.js
+++ qv-interface/vote.js
@@ -1,7 +1,7 @@
 import axios from 'axios' // Axios for requests
 import moment from 'moment' // Moment date parsing
 import Link from 'next/link' // Dynamic links
-import Loader from 'components/loader' // Placeholder loader
+import { Loader } from './Loader' // Placeholder loader
 import Layout from 'components/layout' // Layout wrapper
 import { useRouter } from 'next/router' // Router for URL params
 import { useState, useEffect } from 'react' // State management
