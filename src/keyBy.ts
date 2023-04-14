eea586867354d023dc356d1be4930f8aea14a39d
David Ernst <git@dsernst.com>
2023-04-14 21:54:43 +0300

    Scripts to check db against BoE voter file

--- src/keyBy.ts
+++ src/keyBy.ts
@@ -1,5 +1,5 @@
 /**  This function takes an array of type T and a key of type keyof T (which means that the key must be a property of type T). It returns an object where each key is a string representation of the value of the specified property (key) of each item in the array, and the corresponding value is the item itself. */
-function keyBy<T>(array: T[], key: keyof T): { [key: string]: T } {
+export function keyBy<T>(array: T[], key: keyof T): { [key: string]: T } {
   return array.reduce((result, item) => {
     result[String(item[key])] = item
     return result
