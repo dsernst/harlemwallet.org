db5b7e6972b81245141eb271ef9dc42bba151163
David Ernst <git@dsernst.com>
2023-04-19 23:54:36 +0300

    /voice: Add projects

---
+++ src/projects.ts
@@ -0,0 +1,59 @@
+type Project_Title = string
+type Allocation_Amount = number
+type Project_Description = string
+
+export const projects: [Project_Title, Allocation_Amount, Project_Description][] = [
+  [
+    'Timbale Terrace',
+    250_000,
+    '341 mixed-income housing project, 100 supportive housing units included developed by the Lantern Organization. Located at Park Avenue in between E 118th and E 119th Streets.',
+  ],
+  [
+    'The Baxter',
+    1_000_000,
+    "Broadway Housing Community plans to develop between 150-175 units of affordable housing over the next 5 years at two properties: 222-226 W 145th Street and 673 St Nicholas Avenue. All units will be below 80% AMI. Tenants of the project will be able to access BHC's tenant services, educational programs, and cultural arts programs.",
+  ],
+  [
+    'PS 46',
+    200_000,
+    'The need is a refurbishment or renovation of the floors, the walls, the sound system, the stage, the lighting system, and the seating in the auditorium.',
+  ],
+  [
+    'PS 46',
+    100_000,
+    'The ask is the replacement of the gym floor with regulation flooring; the replacement of the basketball hoops; and the addition of climbing wall and adaptive, sensory gym equipment for special needs students.',
+  ],
+  [
+    'Dance Theater of Harlem',
+    250_000,
+    "Funding to support the installation of a new HVAC, boiler, and fire panel systems replacing the organization's energy inefficient and outdated systems.",
+  ],
+  ['PS 130', 175_000, 'Classroom upgrades to technology as part of the Advancing Classroom Technologies Initiative.'],
+  [
+    'Courtney Callendar Park',
+    250_000,
+    'Reworking of plumbing system to aid in the construction of public restrooms in the park.',
+  ],
+  [
+    'Harlem River Park',
+    250_000,
+    'Funding to support climate resiliency efforts and park revitalization projects at Harlem River Park.',
+  ],
+  [
+    'Marcus Garvey Playground',
+    275_000,
+    'Funding to support the construction of a green playground in Marcus Garvey Park.',
+  ],
+  ['PS 149', 475_000, 'Funding to support auditorium and gym updates for PS 149.'],
+  [
+    'Frederick Samuels Community Center',
+    250_000,
+    'Funding to support upgrades to the kitchen, gym, and classroom facilities in the Community Center.',
+  ],
+  [
+    'Parks Department',
+    50_000,
+    'Funding to support composting bins, mural construction, and revitalization of the Abyssinian Tot-Lot at 130 W 139th Street.',
+  ],
+]
+// Total Number of Projects: 11	 / $3,525,000.00
