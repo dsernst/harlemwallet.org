8b9f3f3a1c7d96fba5a1f3fabad60dabde47c975
alexrandaccio <alex@radicalxchange.org>
2023-04-28 13:56:54 -0400

    make instructions collapsible

--- pages/voice.tsx
+++ pages/voice.tsx
@@ -5,6 +5,15 @@ import { QVInterface } from '../src/qv-interface/QVInterface'
 
 const total_allocation_amount = projects.reduce((sum, [_, amount]) => sum + amount, 0)
 
+/** Toggle show/hide full instructions */
+const toggleInstructions = () => {
+  const collapsible = document.getElementById('collapsible') as HTMLElement;
+  const button = document.getElementById('toggle-button') as HTMLButtonElement;
+
+  collapsible.classList.toggle('hidden');
+  button.textContent = collapsible.classList.contains('hidden') ? 'Show full instructions' : 'Collapse full instructions';
+}
+
 const VoicePage: NextPage = () => {
   return (
     <div className="absolute inset-0 min-h-screen overflow-scroll bg-eggplant-purple">
@@ -21,8 +30,17 @@ const VoicePage: NextPage = () => {
 
       <div className="summary-text text-[18px] leading-1.5 text-white/60">
         <p>Welcome to this pilot of the Harlem Community Wallet participatory budgeting process! Below, you will see a list of spending proposals for District 9. If you want to express support for a spending proposal, click “+”. You can cast more than one vote for a spending proposal by clicking “+” multiple times. If you change your mind, just click “-” to take back votes.</p>
-        <p className="mt-4">Placing votes costs voice credits. When you run out of voice credits, you can’t place any more votes. The cost increases as you add more votes to a single option. So one vote costs 1 credit, two votes cost 4 credits, three votes cost 9 credits, and so on. This means that if you want to strongly support a proposal you can, but it will be very expensive.</p>
-        <p className="mt-4">Spread your votes across all the options that you want to support. When you are finished, click the Submit button to finalize your votes. Your votes will be added up with everyone else’s, resulting in a clear and detailed representation of the group’s preferences.</p>
+        <div id="collapsible" className="hidden">
+          <p className="mt-4">Placing votes costs voice credits. When you run out of voice credits, you can’t place any more votes. The cost increases as you add more votes to a single option. So one vote costs 1 credit, two votes cost 4 credits, three votes cost 9 credits, and so on. This means that if you want to strongly support a proposal you can, but it will be very expensive.</p>
+          <p className="mt-4">Spread your votes across all the options that you want to support. When you are finished, click the Submit button to finalize your votes. Your votes will be added up with everyone else’s, resulting in a clear and detailed representation of the group’s preferences.</p>
+        </div>
+        <button
+          id="toggle-button"
+          className="mt-4 hover:opacity-80"
+          onClick={() => toggleInstructions()}
+        >
+          <span>Show Full Instructions</span>
+        </button>
       </div>
 
       {/* Intro section */}
