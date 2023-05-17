43526ffcfb5217643a6e281dd6f23690e5adfd54
David Ernst <git@dsernst.com>
2023-05-17 15:59:03 -0700

    Assign authCodes to Voters w/ Lob addresses

---
+++ pages/api/assign-auth-codes.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { firebase } from './_firebase'
+import registrations from '../../db/registrations.json'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (process.env.VERCEL) return res.status(401).send('Unauthorized')
+
+  const db = firebase.firestore()
+  const batch = db.batch()
+  const registrationsCol = db.collection('registrations')
+
+  let results: unknown[] = []
+
+  registrations.map((entry, i) => {
+    if (!entry.lobAddress) return
+    if (entry.authCode) return
+
+    const initials = entry.name
+      .split(' ')
+      .map((word) => word[0])
+      .join('')
+      .toUpperCase()
+
+    // Random 4-digit number, between 1000 - 9999
+    const random = Math.floor(Math.random() * 9000) + 1000
+
+    const authCode = `${initials}-${random}`
+    results.push([i, entry.id, authCode])
+
+    batch.update(registrationsCol.doc(entry.id), { authCode })
+  })
+
+  batch.commit()
+
+  return res.status(200).json({ results, count: results.length })
+}
